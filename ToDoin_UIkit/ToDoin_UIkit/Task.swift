3e9a1d539d47180e1d6795fe9f75a3109465c096
CHOIY3SUNG <choiyesung2848@naver.com>
2022-09-06 22:32:11 +0900

    commit

---
+++ ToDoin_UIkit/ToDoin_UIkit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  ToDoin_UIkit
+//
+//  Created by Y3SUNG on 2022/09/05.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var time: Date
+    var iswidget: Bool
+}

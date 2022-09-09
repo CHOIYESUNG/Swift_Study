e2caaeb836ab53b3c8eb2cd4159d8bfcde04de5b
CHOIY3SUNG <choiyesung2848@naver.com>
2022-09-09 20:19:32 +0900

    commit

---
+++ ToDoin_UIkit/ToDoin_UIkit/PlusViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PlusViewController.swift
+//  ToDoin_UIkit
+//
+//  Created by Y3SUNG on 2022/09/09.
+//
+
+import UIKit
+
+class PlusViewController: UIViewController {
+    @IBOutlet weak var inputTodo: UITextField!
+    @IBOutlet weak var showWidget: UISwitch!
+    @IBOutlet weak var deadlineDate: UIDatePicker!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var tasks = [Task]() {
+        didSet {
+            self.tabSaveButton()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        saveButton.addTarget(self, action: #selector(tabSaveButton), for: .touchUpInside)
+    }
+    
+    @objc
+    func tabSaveButton() {
+        debugPrint("\(inputTodo.text!)")
+        debugPrint("\(saveButton.isSelected)")
+        debugPrint("\(deadlineDate.date)")
+        let data = self.tasks.map  {
+            [
+                inputTodo.text: $0.title,
+                deadlineDate.date: $0.time
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "tasks")
+        
+        debugPrint(data)
+    }
+    
+}
--- ToDoin_UIkit/ToDoin_UIkit/ViewController.swift
+++ ToDoin_UIkit/ToDoin_UIkit/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var EditButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
